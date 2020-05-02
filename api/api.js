4d47b1809ac43f662e748adb8aeadee7f3766782
gauthier <gauthier.lrnt@gmail.com>
2020-05-02 20:40:08 +0200

    fix: serverless pricer

--- api/api.js
+++ api/api.js
@@ -2,6 +2,6 @@ import './scraper_bourso.js'
 
 module.exports = async (req, res) => {
   console.log(req.query.isin)
-  if(req.query.isin) res.end(JSON.stringify(await get_price2(req.query.isin)))
+  if(req.query.isin) res.end(JSON.stringify(await get_price(req.query.isin)))
   res.end('url must contains ?isin=XXXXXXXXXXXX (with X is an isin)')
 }
--- api/scraper_bourso.js
+++ api/scraper_bourso.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const fetch = require('node-fetch')
 const firebase = require('firebase-admin')
 
