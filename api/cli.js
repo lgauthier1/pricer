622122389f50f32debbad0d3a9b2e9e423416329
gauthier <gauthier.lrnt@gmail.com>
2020-05-03 10:49:09 +0200

    feat: remove cli

--- api/cli.js
+++
@@ -1,9 +0,0 @@
-const isin = process.argv[2]
-require('./scraper_bourso.js')
-
-const cli = async () => {
-  if (!isin) return 'missing mandatory parameter'
-  return await get_price(isin)
-}
-
-cli().then(console.log)
