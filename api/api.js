07ba4d6de725ad5d78ac14d0344ae139035ea2d9
gauthier <gauthier.lrnt@gmail.com>
2020-05-03 17:02:23 +0200

    feat: + get price historic

--- api/api.js
+++ api/api.js
@@ -1,7 +1,6 @@
 import './scraper_bourso.js'
 
 module.exports = async (req, res) => {
-  console.log(req.query.isin)
-  if(req.query.isin) res.end(JSON.stringify(await get_price(req.query.isin)))
-  res.end('url must contains ?isin=XXXXXXXXXXXX (with X is an isin)')
+  if (!req.query.isin) res.end('url must contains ?isin=XXXXXXXXXXXX (with X is an isin)')
+   res.end(JSON.stringify(await get_price(req.query.isin)))
 }
