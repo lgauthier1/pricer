e84e681e6086c48d5596e6cbe6d9ac4f4785523b
gauthier <gauthier.lrnt@gmail.com>
2020-05-03 10:47:46 +0200

    fix: prettier code

--- api/scraper_bourso.js
+++ api/scraper_bourso.js
@@ -1,13 +1,16 @@
 const fetch = require('node-fetch')
 const firebase = require('firebase-admin')
 
-const serviceAccount = require("./pricer.json");
+const serviceAccount = require('./pricer.json')
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://pricer-c75a5.firebaseio.com"
+  databaseURL: 'https://pricer-c75a5.firebaseio.com',
 })
 const database = firebase.database()
-const get = async (key) => { return await database.ref(key).once('value').then( (snapshot) => { return snapshot.val() }) }
+const get = async key => {return await database.ref(key).once('value').then(snapshot => {
+      return snapshot.val()
+    })
+}
 const set = async (key, value) => {
   const update = {}
   update[key] = value
@@ -18,79 +21,73 @@ require('./raw.js')
 Object.extend(true)
 
 const markets = {
-  'cac40': {
-    'url': 'https://www.boursorama.com/bourse/actions/cotations/',
-    'max_page': 12,
-    'pattern1': new RegExp(/href="\/cours\/.*\/"[\s]{2,}title=.*/, "g"),
-    'pattern2': new RegExp(/href="\/cours\/.*\/"[\s]/, "g"),
+  cac40: {
+    url: 'https://www.boursorama.com/bourse/actions/cotations/',
+    max_page: 12,
+    pattern1: new RegExp(/href="\/cours\/.*\/"[\s]{2,}title=.*/, 'g'),
+    pattern2: new RegExp(/href="\/cours\/.*\/"[\s]/, 'g'),
   },
-  'etf': {
-    'url': 'https://www.boursorama.com/bourse/trackers/palmares/',
-    'max_page': 40,
-    'pattern1': new RegExp(/href=".*\/cours\/.*\/"[\s]{2,}title=.*/, "g"), // A regrouper avec autre
-    'pattern2': new RegExp(/href=".*\/cours\/.*\/"[\s]/, "g"),
-  }
+  etf: {
+    url: 'https://www.boursorama.com/bourse/trackers/palmares/',
+    max_page: 40,
+    pattern1: new RegExp(/href=".*\/cours\/.*\/"[\s]{2,}title=.*/, 'g'), // A regrouper avec autre
+    pattern2: new RegExp(/href=".*\/cours\/.*\/"[\s]/, 'g'),
+  },
 }
 
-const parse_fiche_valeur = async (url) => {
-  try{
-    const fiche_valeur = await (await fetch('https://www.boursorama.com/' + url, {redirect: 'follow'})).text()
+const parse_fiche_valeur = async url => {
+  try {
+    const fiche_valeur = await (await fetch('https://www.boursorama.com/' + url, { redirect: 'follow' })).text()
     const isin = fiche_valeur.match(/c-faceplate__isin">[A-Z0-9]{12}/g)[0].split('>')[1]
     const price = fiche_valeur.replace(/\n|\r/g, '').match(/<span class="c-instrument c-instrument--last" data-ist-last>.*?</)[0].match(/[0-9]*\.[0-9]*/)[0]
     return { url, isin, price }
-  }
-  catch(e){
-    return { url, isin:'error', price:'error'}
+  } catch (e) {
+    return { url, isin: 'error', price: 'error' }
   }
 }
 
-module.export = get_symbols = async (market) => {
+module.export = get_symbols = async market => {
   const { url, max_page, pattern1, pattern2 } = markets[market]
-  const raw_urls = await Promise.all([...Array(max_page).keys()].map( async (page) => {
-    const content = await (await fetch(url + 'page-' + page)).text()
-    return content.match(pattern1).unique().filter(d => !d.includes("Cours "))
-  }))
+  const raw_urls = await Promise.all(
+    [...Array(max_page).keys()].map(async page => {
+      const content = await (await fetch(url + 'page-' + page)).text()
+      return content.match(pattern1).unique().filter(d => !d.includes('Cours '))
+    }),
+  )
 
-  const symbols = await Promise.all(raw_urls.flat().unique().map(async d => {
-    try{
-      const url = d.match(pattern2)[0].replace('href="/','').replace('/"\n','')
-      const label = d.match(/title=".*"/g)[0].replace('title="', '').replace('"','')
-      const fiche_valeur =  await parse_fiche_valeur(url)
-      return  {...fiche_valeur, label, market}
-    }catch(e){
-      console.log(e)
-    }
-  }))
+  const symbols = await Promise.all(
+    raw_urls.flat().unique().map(async d => {
+        try {
+          const url = d.match(pattern2)[0].replace('href="/', '').replace('/"\n', '')
+          const label = d.match(/title=".*"/g)[0].replace('title="', '').replace('"', '')
+          const fiche_valeur = await parse_fiche_valeur(url)
+          return { ...fiche_valeur, label, market }
+        } catch (e) {
+          console.log(e)
+        }
+      }),
+  )
   return symbols
 }
 
 module.export = sync_firebase = async () => {
   const cac40 = await get_symbols('cac40')
-  const etf = await  get_symbols('etf')
-  await set('public/referentiel', {...cac40, ...etf})
+  const etf = await get_symbols('etf')
+  await set('public/referentiel', { ...cac40, ...etf })
 }
 
-module.export = get_symbol = async (isin) => {
-    return await get('public/referentiel/{}'.format(isin))
+module.export = get_symbol = async isin => {
+  return await get('public/referentiel/{}'.format(isin))
 }
 
-module.export = get_price = async (isin) => {
+module.export = get_price = async isin => {
   const symbol = await get_symbol(isin)
   return {
-      date: new Date().format(),
-      price: +(await parse_fiche_valeur(symbol.url)).price ,
-      label: symbol.label,
-      }
+    date: new Date().format(),
+    price: +(await parse_fiche_valeur(symbol.url)).price,
+    market: symbol.market,
+    label: symbol.label
+  }
 }
-
-module.export = get_price2 = async (isin) => {
-  // const symbol = await get_symbol(isin)
-  return {
-      date: new Date().format(),
-      price: 10,
-      label: 'LGA',
-      }
-}
-
 // TO DO:
 // export 1x at end.
--- api/test.js
+++ api/test.js
@@ -1,16 +1,16 @@
 import './scraper_bourso.js'
 
-const display_test = (label ,status) => {
-  const FgRed = "\x1b[31m"
-  const FgGreen = "\x1b[32m"
-  const Reset = "\x1b[0m"
-  const green = (str) => [FgGreen, str, Reset].join(' ')
-  const red = (str) => [FgRed, str, Reset].join(' ')
+const display_test = (label, status) => {
+  const FgRed = '\x1b[31m'
+  const FgGreen = '\x1b[32m'
+  const Reset = '\x1b[0m'
+  const green = str => [FgGreen, str, Reset].join(' ')
+  const red = str => [FgRed, str, Reset].join(' ')
   console.log(label, status ? green('OK') : red('KO'))
 }
 
-const test_build_symbols = async (market) => {
-  try{
+const test_build_symbols = async market => {
+  try {
     const start = new Date()
     const data = await get_symbols(market)
     const end = new Date()
@@ -18,7 +18,7 @@ const test_build_symbols = async (market) => {
     display_test('Build {} Symbols (in {})'.format(market, duration), true)
     const missing = data.flat().filter(d => d.isin === 'error' || d.url === 'error').length
     display_test('{} Symbols with error {} / {}:'.format(market, missing.toString(), data.flat().length), missing == 0)
-  }catch(e){
+  } catch (e) {
     console.log(e)
     display_test('Build {} Symbols'.format(market), false)
   }
@@ -27,26 +27,26 @@ const test_build_symbols = async (market) => {
 const run_tests = async () => {
   await test_build_symbols('cac40')
   await test_build_symbols('etf')
-  
-  display_test('Check FR0000120271 (CAC40):', await get_symbol('FR0000120271') !== null)
-  display_test('Check FR0000066672 (CAC40):', await get_symbol('FR0000066672') !== null)
-  display_test('Check FR0000133308 (CAC40):', await get_symbol('FR0000133308') !== null)
-  display_test('Check FR0004163111 (CAC40):', await get_symbol('FR0004163111') !== null)
-  display_test('Check FR0010208488 (CAC40):', await get_symbol('FR0010208488') !== null)
+
+  display_test('Check FR0000120271 (CAC40):', (await get_symbol('FR0000120271')) !== null)
+  display_test('Check FR0000066672 (CAC40):', (await get_symbol('FR0000066672')) !== null)
+  display_test('Check FR0000133308 (CAC40):', (await get_symbol('FR0000133308')) !== null)
+  display_test('Check FR0004163111 (CAC40):', (await get_symbol('FR0004163111')) !== null)
+  display_test('Check FR0010208488 (CAC40):', (await get_symbol('FR0010208488')) !== null)
   // (OST => LU1681047079)
-  display_test('Check FR0010878033 (CAC40):', await get_symbol('FR0010878033') !== null)
+  display_test('Check FR0010878033 (CAC40):', (await get_symbol('FR0010878033')) !== null)
   // FR0013457272 => KO (FDJ => FR0013451333)
-  display_test('Check FR0013457272 (CAC40):', await get_symbol('FR0013457272') !== null)
-  display_test('Check FR0004035913 (ETF)  :', await get_symbol('FR0004035913') !== null)
-  display_test('Check LU1681047079 (ETF)  :', await get_symbol('LU1681047079') !== null)
-  display_test('Check FR0013041530 (ETF)  :', await get_symbol('FR0013041530') !== null)
-  display_test('Check FR0011869353 (ETF)  :', await get_symbol('FR0011869353') !== null)
+  display_test('Check FR0013457272 (CAC40):', (await get_symbol('FR0013457272')) !== null)
+  display_test('Check FR0004035913 (ETF)  :', (await get_symbol('FR0004035913')) !== null)
+  display_test('Check LU1681047079 (ETF)  :', (await get_symbol('LU1681047079')) !== null)
+  display_test('Check FR0013041530 (ETF)  :', (await get_symbol('FR0013041530')) !== null)
+  display_test('Check FR0011869353 (ETF)  :', (await get_symbol('FR0011869353')) !== null)
 
   const price_cac40 = await get_price('FR0000120271')
-  display_test('Check get_price FR0000120271 (CAC40): {}'.format(price_cac40), typeof(+price_cac40) === 'number')
+  display_test('Check get_price FR0000120271 (CAC40): {}'.format(price_cac40), typeof +price_cac40 === 'number')
 
   const price_etf = await get_price('FR0011869353')
-  display_test('Check get_price FR0011869353 (ETF): {}'.format(price_etf.price), typeof(+price_etf.price) === 'number')
+  display_test('Check get_price FR0011869353 (ETF): {}'.format(price_etf.price), typeof +price_etf.price === 'number')
 }
 
 run_tests()
