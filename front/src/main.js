ccc3fe665ff4c66f0d4636bb7043863dee09fe4e
gauthier <gauthier.lrnt@gmail.com>
2020-05-21 20:23:59 +0200

    feat: Prepare Architecture + PUG

--- front/src/main.js
+++ front/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 // Gestion des composants:
+import NavTop from './components/layout/NavTop.vue'
 import Screen1 from './screens/screen1.vue'
 
 const components = {
+  NavTop,
   Screen1
 }
 Object.keys(components).map(name => Vue.component(name, components[name]))
