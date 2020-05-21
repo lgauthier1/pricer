245efed957c6dc5b835c3c13e7a740e8535c44c7
gauthier <gauthier.lrnt@gmail.com>
2020-05-21 20:30:39 +0200

    fix: Architecture Setup

---
+++ front/src/components/layout/NavTop.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  .nav
+    router-link(to="/") Home |
+    router-link(to="/test/screen1") Screen1
+</template>
+<style>
+.nav {width: 100%; background: blue;}
+.nav a {color: white;}
+</style>
